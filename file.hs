f73b508d6f1acdcc1956637964a56467ec6ea608
frt34 <dasadasad38@gmail.com>
2025-02-18 14:00:01 +0700

    file.hs

--- file.hs
+++ file.hs
@@ -1,11 +1,6 @@
 main :: IO ()
-main = putStrLn "Hello, World!"
-main = putStrLn "hai kang ini cuma sample jangan bawa serius"
-main = putStrLn "godaaaaaam"
-
-
-
-
-
-
-main = "ngapain masih liat njir"
+main = do
+    putStrLn "Hello, World!"
+    putStrLn "hai kang ini cuma sample jangan bawa serius"
+    putStrLn "godaaaaaam"
+    putStrLn "ngapain masih liat njir"
